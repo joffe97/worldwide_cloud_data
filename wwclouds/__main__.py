5b27b4cd2f07e1ee68bc11953fbffd84e3652c62
Joachim Andreassen <joachima94@gmail.com>
2022-05-12 22:40:36 +0200

    Removed unused code

--- wwclouds/__main__.py
+++ wwclouds/__main__.py
@@ -13,11 +13,4 @@ from wwclouds.domains.product import ProductCreator
 
 if __name__ == '__main__':
     from datetime import datetime
-    ProductCreator.from_args(
-        # utctime=datetime(2022, 2, 14, 9, 25).timestamp()
-        # utctime=datetime.utcnow().timestamp()
-        # utctime=datetime(2020, 12, 14, 19, 00).timestamp()
-        # utctime=datetime(2022, 2, 13, 14, 25).timestamp()
-        # utctime=datetime(2022, 4, 30, 11, 00).timestamp()
-        utctime=datetime(2022, 5, 1, 14, 00).timestamp()
-    ).create_products()
+    ProductCreator.from_args().create_products()
