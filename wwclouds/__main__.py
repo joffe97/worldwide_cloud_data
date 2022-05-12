df89932543f14d966df094b4dd3263f43cd7587e
Joachim Andreassen <joachima94@gmail.com>
2022-05-12 16:56:15 +0200

    Changed time

--- wwclouds/__main__.py
+++ wwclouds/__main__.py
@@ -17,5 +17,7 @@ if __name__ == '__main__':
         # utctime=datetime(2022, 2, 14, 9, 25).timestamp()
         # utctime=datetime.utcnow().timestamp()
         # utctime=datetime(2020, 12, 14, 19, 00).timestamp()
-        utctime=datetime(2022, 2, 13, 14, 25).timestamp()
+        # utctime=datetime(2022, 2, 13, 14, 25).timestamp()
+        # utctime=datetime(2022, 4, 30, 11, 00).timestamp()
+        utctime=datetime(2022, 5, 1, 14, 00).timestamp()
     ).create_products()
